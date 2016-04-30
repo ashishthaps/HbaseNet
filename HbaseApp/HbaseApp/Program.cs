a74e185dad7ef979ec238c44e103bea1bcd4e376
ashishthaps@hotmail.com <ashishth@microsoft.com>
2016-04-29 20:43:13 -0700

    Fixed for Visual Studio 2015

--- HbaseApp/HbaseApp/Program.cs
+++ HbaseApp/HbaseApp/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.HBase.Client;
 using Microsoft.HBase;
 using org.apache.hadoop.hbase.rest.protobuf.generated;
+using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+
 
 namespace HbaseApp
 {
@@ -13,14 +15,26 @@ namespace HbaseApp
     {
         static void Main(string[] args)
         {
-            var creds = new ClusterCredentials("","","");
-            var client = new HBaseClient(creds);
-            var tableschema = new TableSchema();
-            tableschema.name = "ReallyBigtable";
-            tableschema.columns.Add(new ColumnSchema() { name = "d" });
-            tableschema.columns.Add(new ColumnSchema() { name = "f" });
-            client.CreateTableAsync(tableschema).Wait();
+            Console.ReadLine();
 
+            try
+            {
+                var creds = new ClusterCredentials(new Uri("https://x.azurehdinsight.net"), "", "");
+                var client = new HBaseClient(creds);
+                var version = client.GetVersionAsync().Result;
+                Console.WriteLine(version);
+                var tableschema = new TableSchema();
+                tableschema.name = "ReallyBigtable";
+                tableschema.columns.Add(new ColumnSchema() { name = "d" });
+                tableschema.columns.Add(new ColumnSchema() { name = "f" });
+                client.CreateTableAsync(tableschema).Wait();
+            }
+            catch(Exception ex)
+            {
+
+                Console.WriteLine(ex.Message.ToString());
+            }
+                Console.ReadLine();
 
 
 
